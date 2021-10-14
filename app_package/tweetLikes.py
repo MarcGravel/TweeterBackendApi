bb82f5725282bcf6b521c88906bdf2cb3e634014
Marc Gravel <gravel.m@outlook.com>
2021-10-13 17:46:42 -0700

    tweet-likes/GET completed

--- app_package/tweetLikes.py
+++ app_package/tweetLikes.py
@@ -18,6 +18,7 @@ def api_tweet_likes():
 
         if request.method == 'GET':
             params = request.args
+            print(len(params.keys()))
             
             #checks length of request. 2 lengths accepted. 
             #no data returns all likes
@@ -25,20 +26,36 @@ def api_tweet_likes():
                 cursor.execute("SELECT t.tweet_id, u.id, u.username FROM tweet_like t INNER JOIN user u ON t.user_id = u.id")
                 all_tweet_likes = cursor.fetchall()
                 all_likes_list = []
-
-                #adds all likes to a key formatted dict to return
-                for l in all_tweet_likes:
-                    like = pop_tweet_like(l)
-                    all_likes_list.append(like)
-                
-                return Response(json.dumps(all_likes_list), mimetype="application/json", status=200)
+                    #skip next elif/else statments to continue
 
             #id returns all likes of that tweet id
-            if len(params.keys()) and {"tweetId"} <= params.keys():
-                pass
+            elif len(params.keys()) == 1 and {"tweetId"} <= params.keys():
+                tweet_id = params.get("tweetId")
+
+                #checks if valid positive integer 
+                if tweet_id.isdigit() == False:
+                    return Response("Not a valid id number", mimetype="text/plain", status=400)
+
+                #checks if tweet_id exists in db
+                cursor.execute("SELECT EXISTS(SELECT * FROM tweet WHERE id=?)", [tweet_id])
+                check_id_valid = cursor.fetchone()[0]
+
+                if check_id_valid == 1:
+                    cursor.execute("SELECT t.tweet_id, u.id, u.username FROM tweet_like t INNER JOIN user u ON t.user_id = u.id \
+                                    WHERE t.tweet_id=?", [tweet_id])
+                    all_tweet_likes = cursor.fetchall()
+                    all_likes_list = []
+                        #skip else statments to continue
+                else:
+                    return Response("Tweet id does not exist", mimetype="text/plain", status=400)
             else:
                 return Response("Incorrect Json data sent", mimetype='text/plain', status=400)
-
+            
+            # for either request above return all likes in a key formatted dict
+            for l in all_tweet_likes:
+                like = pop_tweet_like(l)
+                all_likes_list.append(like)
+            return Response(json.dumps(all_likes_list), mimetype="application/json", status=200)
 
         elif request.method == 'POST':
             data = request.json
