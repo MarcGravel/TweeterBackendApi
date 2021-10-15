f04ea1563980912fdaa4f1412d4f3a5b7a3e0872
Marc Gravel <gravel.m@outlook.com>
2021-10-14 17:27:16 -0700

    C:/Program Files/Git/followers queries updated

--- app_package/followers.py
+++ app_package/followers.py
@@ -1,5 +1,6 @@
 from app_package import app
 from app_package.dataManFunctions import pop_dict_query
+from app_package.queryFunctions import db_fetchall_args, db_index_fetchone, db_fetchone, db_fetchall, db_commit
 import dbcreds
 import mariadb
 from flask import request, Response
@@ -7,70 +8,40 @@ import json
 
 @app.route('/api/followers', methods=['GET'])
 def api_followers():
-    try:
-        conn = mariadb.connect(
-                        user=dbcreds.user,
-                        password=dbcreds.password,
-                        host=dbcreds.host,
-                        port=dbcreds.port,
-                        database=dbcreds.database
-                        )
-        cursor = conn.cursor()
+    if request.method == 'GET':
+        params = request.args
 
-        if request.method == 'GET':
-            params = request.args
+        #checks that only one param is sent
+        if len(params) == 1:
+            if {"userId"} <= params.keys():
+                param_id = params.get("userId")
 
-            #checks that only one param is sent
-            if len(params) == 1:
-                if {"userId"} <= params.keys():
-                    param_id = params.get("userId")
+                #checks if valid positive integer
+                if param_id.isdigit() == False:
+                    return Response("Not a valid id number", mimetype="text/plain", status=400)
 
-                    #checks if valid positive integer
-                    if param_id.isdigit() == False:
-                        return Response("Not a valid id number", mimetype="text/plain", status=400)
+                #checks if id exists as a user id. Returns bool 1 or 0
+                check_id_valid = db_index_fetchone("SELECT EXISTS(SELECT * FROM user WHERE id=?)", [param_id])
 
-                    #checks if id exists as a user id. Returns bool 1 or 0
-                    cursor.execute("SELECT EXISTS(SELECT * FROM user WHERE id=?)", [param_id])
-                    check_id_valid = cursor.fetchone()[0]
+                #handle response
+                if check_id_valid == 1:
+                    all_followers = db_fetchall_args("SELECT id, email, username, bio, birthdate, image_url, banner_url \
+                                    FROM user u INNER JOIN follows f ON u.id = f.follower WHERE f.followed=?", [param_id])
+                    all_followers_list = []
 
-                    #handle response
-                    if check_id_valid == 1:
-                        cursor.execute("SELECT id, email, username, bio, birthdate, image_url, banner_url \
-                                        FROM user u INNER JOIN follows f ON u.id = f.follower WHERE f.followed=?", [param_id])
-                        all_followers = cursor.fetchall()
-                        all_followers_list = []
+                    #adds all valid users to a key formatted dict to return
+                    for a in all_followers:
+                        user = pop_dict_query(a)
+                        all_followers_list.append(user)
 
-                        #adds all valid users to a key formatted dict to return
-                        for a in all_followers:
-                            user = pop_dict_query(a)
-                            all_followers_list.append(user)
+                    return Response(json.dumps(all_followers_list), mimetype="application/json", status=200)
 
-                        return Response(json.dumps(all_followers_list), mimetype="application/json", status=200)
-
-                    else:
-                        return Response("User id does not exist", mimetype="text/plain", status=400)
                 else:
-                    return Response("Incorrect key submitted.", mimetype='text/plain', status=400)
+                    return Response("User id does not exist", mimetype="text/plain", status=400)
             else:
-                return Response("Incorrect amount of data sent", mimetype="text/plain", status=400)
+                return Response("Incorrect key submitted.", mimetype='text/plain', status=400)
         else:
-            print("Something went wrong, bad request method")
-            return Response("Method Not Allowed", mimetype='text/plain', status=405)
-            
-
-    except mariadb.DataError:
-        print("Something is wrong with your data")
-        return Response("Something is wrong with the data", mimetype='text/plain', status=404)
-    except mariadb.OperationalError:
-        print("Something is wrong with your connection")
-        return Response("Something is wrong with the connection", mimetype='text/plain', status=500)
-    except:
-        print("Something went wrong")
-        return Response("Something went wrong", status=500)
-    
-    finally:
-        if (cursor != None):
-            cursor.close()
-        if (conn != None):
-            conn.rollback()
-            conn.close()
+            return Response("Incorrect amount of data sent", mimetype="text/plain", status=400)
+    else:
+        print("Something went wrong, bad request method")
+        return Response("Method Not Allowed", mimetype='text/plain', status=405)
